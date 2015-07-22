24b14504cb6220f1fbc9455f72760f6505df0c03
amal-roshan <scaryamal@gmail.com>
2015-07-22 19:15:11 +0530

    Create SizeParameters.cs

---
+++ SizeParameters.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pingpong3
+{
+    public static class SizeParameters
+    {
+        public const int SCREEN_HEIGHT = 650;
+        public const int SCREEN_WIDTH = 650;
+        public const int BAT_WIDTH = 20;
+        public const int BAT_HEIGHT = 50;
+        public const int BALL_WIDTH = 20;
+        public const int BALL_HEIGHT = 20;
+        public const int STEP = 40;
+        public const int BALL_SPEEDX = 3;
+        public const int BALL_SPEEDY = 3;
+        public const int WINNING_SCORE = 3;
+        public const int GAME_INTERVAL = 10;
+    }
+}
